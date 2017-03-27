edb9e6425f63f38db7954be3cc3a44244860b690
loes.kenis <loes.kenis@B209-09>
2017-03-27 11:33:20 +0200

    Added classes and started on Game class

---
+++ SpaceInvadersClone/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace SpaceInvadersClone
+{
+    class Game
+    {
+        private User user;
+        private Alien alien;
+
+        private Lasers lasers;
+        private Bombs bomb;
+
+        private Canvas gameCanvas;
+
+        private DispatcherTimer animationTimer;
+        private DispatcherTimer bombTimer;
+
+        public Game()
+        {
+            
+        }
+    }
+}
