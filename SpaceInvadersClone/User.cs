5cffb2ad3188e20f4c8ae1c8507360cd43144189
loes.kenis <loes.kenis@B209-09>
2017-03-27 11:55:29 +0200

    Finished Sprite Class

--- SpaceInvadersClone/User.cs
+++ SpaceInvadersClone/User.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace SpaceInvadersClone
 {
     class User
     {
+        private Image userImage;
+
+        public void Move()
+        {
+
+        }
+        public void DisplayOn()
+        {
+
+        }
     }
 }
